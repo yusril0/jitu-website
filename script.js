9462feb9b0afb53ddb1649867d3cf2d92700942f
yusril0 <150217831+yusril0@users.noreply.github.com>
2023-11-08 12:09:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change slide every 2 seconds
+}
+
+// Run the slideshow when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+    showSlides();
+});
